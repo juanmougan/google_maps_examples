243528e72c91a925350cec09a154261e7db81bc0
Juan Manuel Mougan <juanmougan@gmail.com>
2017-02-22 01:08:38 -0300

    Map is showing OK

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(express.static('public'));
 app.get('/maps_api_key', function (req, res) {
   const API_KEY = String(process.env.API_KEY)
   console.log("Sending API KEY: " + API_KEY)
+  res.type('text/plain')
   res.send(API_KEY)
 })
 
